93331115667c66be39862250dd256db1a43f3800
gibstock <gibstock@gmail.com>
2021-06-24 16:57:51 -0700

    changed port

--- db.js
+++ db.js
@@ -8,4 +8,4 @@ const MONGO_DB = 'blog-one';
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
