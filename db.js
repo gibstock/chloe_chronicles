12ed7327e0b6c03dab94726a23e77a35848f6667
gibstock <gibstock@gmail.com>
2021-06-24 16:54:50 -0700

    changed port

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const MONGO_USERNAME = 'andre';
 const MONGO_PASSWORD = '1Duke1Echo';
 const MONGO_HOSTNAME = 'localhost';
-const MONGO_PORT = '5000';
+const MONGO_PORT = '27017';
 const MONGO_DB = 'blog-one';
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
